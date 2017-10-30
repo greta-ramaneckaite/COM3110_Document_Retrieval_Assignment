2cec6b74dca80014896785169117ca1174b5eae4
Greta Ramaneckaite <gramaneckaite1@sheffield.ac.uk>
2017-10-30 15:51:13 +0000

    Inverted list indexes

--- my_retriever.py
+++ my_retriever.py
@@ -4,8 +4,18 @@ class Retrieve:
     def __init__(self,index,termWeighting):
         self.index = index
         self.termWeighting = termWeighting
+        self.initList = []
+        self.invertedList = []
+        self.freqCount()
 
     # Method to apply query to index
     def forQuery(self,query):
         return range(1,11)
+
+    def freqCount(self):
+        for x,y in self.index.items():
+            self.initList.append((x, len(y)))
+        self.invertedList = sorted(self.initList, key=lambda x: x[1])
+
+        print(self.invertedList)
 
